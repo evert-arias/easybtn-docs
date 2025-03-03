ce1137a4285a442cd08b50c309a12535e5c1d968
Evert Arias <evert.arias@treve.dev>
2025-03-02 20:39:53 -0500

    feat: update production URL in docusaurus.config.ts for improved deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
     "Arduino library for debouncing momentary contact switches, detect press, release, long press and sequences with event definitions and callbacks.",
 
   // Set the production url of your site here
-  url: "https://evert-arias.github.io/easybtn-docs",
+  url: "https://evert-arias.github.io",
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
